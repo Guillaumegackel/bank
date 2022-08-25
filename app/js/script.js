0d0b506b83f2a8a8701c0ef6bb53f62b5ac428a1
Webme <guillaume.gackel@gmail.com>
2022-08-25 19:21:49 +0200

    cross hamburger ok

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const open = document.getElementById('btnHamburger');
+
+open.addEventListener('click', ()=>{
+	console.log('ca marche');
+
+	if(open.classList.contains('open')){
+		open.classList.remove('open');
+	}
+	else{
+		open.classList.add("open");
+
+	}
+
+})
